404068a555978753a4da2e41c05ba0b1f11e9275
Michał K <uyu1@O2.pl>
2021-02-04 18:34:47 +0100

    Missed file

---
+++ PassWallet.Infrastructure/Services/IPasswordService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PassWallet.Infrastructure.DTO;
+using PassWallet.Infrastructure.DTO.Commands;
+
+namespace PassWallet.Infrastructure.Services
+{
+    public interface IPasswordService
+    {
+        Task<PasswordDto> GetAsync(Guid id);
+        Task<PasswordDto> GetDecryptedPassword(GetDecryptedPassword command, Guid userId);
+        Task<IEnumerable<PasswordDto>> BrowseAsync();
+        Task<IEnumerable<PasswordDto>> BrowseAsync(GetPasswordsByUserCommand command);
+        Task AddAsync(CreatePasswordCommand command, Guid userId);
+        Task UpdateAsync(UpdatePasswordCommand command, Guid userId);
+        Task DeleteAsync(DeletePasswordCommand command);
+    }
+}
